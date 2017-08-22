0d43edaa806ca48dc1492db560b2b94c85972d63
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2017-08-22 13:11:42 +0100

    Create index, create and show actions
    The index, create and show actions have been created.
    The corresponding views have also been created and updated.

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,9 +1,18 @@
 class ObjectivesController < ApplicationController
 
   def index
+    @objectives = Objective.all
+  end
+
+  def create
+    @objective = Objective.new(objective_params)
+
+    @objective.save
+    redirect_to @objective
   end
 
   def show
+    @objective = Objective.find(params[:id])
   end
 
   def new
@@ -16,4 +25,9 @@ class ObjectivesController < ApplicationController
   def delete
   end
 
+  private
+
+  def objective_params
+    params.require(:objective).permit(:title, :description, :deadline)
+  end
 end
