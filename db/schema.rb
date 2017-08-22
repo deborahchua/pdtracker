248d722de05c04c7284d9e76ab27e6e6d608bddb
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2017-08-22 09:34:58 +0100

    Create Roles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170816083455) do
-=======
-ActiveRecord::Schema.define(version: 20170816085708) do
->>>>>>> Create Reviews model
+ActiveRecord::Schema.define(version: 20170822083238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +23,6 @@ ActiveRecord::Schema.define(version: 20170816085708) do
     t.datetime "updated_at",  null: false
   end
 
-<<<<<<< HEAD
-=======
   create_table "reviews", force: :cascade do |t|
     t.string   "question"
     t.text     "comment"
@@ -38,5 +32,10 @@ ActiveRecord::Schema.define(version: 20170816085708) do
     t.datetime "updated_at",       null: false
   end
 
->>>>>>> Create Reviews model
+  create_table "roles", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
