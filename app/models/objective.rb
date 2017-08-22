d6de42c862ac76ab604411383bffc139a88e9fec
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2017-08-22 14:34:32 +0100

    Adding error messages when form is empty
    Error messages will now be shown if the form is empty when creating a new objective

--- app/models/objective.rb
+++ app/models/objective.rb
@@ -1,5 +1,5 @@
 class Objective < ApplicationRecord
-  validates :title, presence: { message: "Title needs to be filled in"}, length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 5 }
   validates :description, presence: true, length: {minimum: 10}
   validates :deadline, presence: true
 
