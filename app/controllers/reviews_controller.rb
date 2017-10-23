4a8d665a4f4e39a29502956bcbc46ac6c6014124
Deborah Chua <deborah.chua@digital.cabinet-office.gov.uk>
2017-10-23 12:50:36 +0100

    Updates to the review and objective controllers
    Reviews can now be edited, and the objective deadline has a user friendly countdown between the current date and the deadline set.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,14 +5,39 @@ def index
 end
 
 def show
-  @objective = Objective.find(params[:id])
-  @reviews = @objective.reviews.all
+  @objective = Objective.find(params[:objective_id])
+  @review = @objective.reviews.find(params[:id])
 end
 
 def create
   @objective = Objective.find(params[:objective_id])
   @review = @objective.reviews.create(review_params)
   redirect_to objective_path(@objective)
+end
+
+def edit
+  @objective = Objective.find(params[:objective_id])
+  @review = @objective.reviews.find(params[:id])
+
+end
+
+def destroy
+  @objective = Objective.find(params[:objective_id])
+  @review = @objective.reviews.find(params[:id])
+  @review.destroy
+
+  redirect_to objectives_path(@objective)
+end
+
+def update
+@objective = Objective.find(params[:objective_id])
+ @review = @objective.reviews.find(params[:id])
+
+  if @review.update(review_params)
+    redirect_to objectives_path
+  else
+    render 'edit'
+  end
 end
 
 private
